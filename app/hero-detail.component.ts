1a0a31eb7b4282c4fc2a9004ee526069e8bc8506
Vivek Tumrukota <vivek@workspan.com>
2016-11-01 17:05:10 -0700

    first commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,38 @@
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: [ 'heroes.component.css' ]
+})
+
+
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
